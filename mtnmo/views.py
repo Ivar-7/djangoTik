5c16df54f5bb1060b6fe76992bf8a64b6268d179
Ivar-7 <leank6071@gmail.com>
2024-04-25 20:10:51 +0300

    try modify 2

--- mtnmo/views.py
+++ mtnmo/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 import requests
 import json
 import uuid
@@ -8,53 +7,99 @@ from basicauth import encode
 class PayClass():
     def __init__(self):
         self.collections_subkey = ""
-
         self.basic_authorisation_collections = ""
         self.collections_apiuser = ""
         self.api_key_collections = ""
-        
         self.environment_mode = "sandbox"
         self.accurl = "https://proxy.momoapi.mtn.com"
         if self.environment_mode == "sandbox":
             self.accurl = "https://sandbox.momodeveloper.mtn.com"
-        if self.environment_mode == "sandbox":
             self.collections_apiuser = str(uuid.uuid4())
+
+    def create_api_user(self):
+        url = f"{self.accurl}/v1_0/apiuser"
+        payload = json.dumps({
+            "providerCallbackHost": "URL of host ie google.com"
+        })
+        headers = {
+            'X-Reference-Id': self.collections_apiuser,
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': self.collections_subkey
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        return response
+
+    def create_api_key(self):
+        url = f"{self.accurl}/v1_0/apiuser/{self.collections_apiuser}/apikey"
+        headers = {
+            'Ocp-Apim-Subscription-Key': self.collections_subkey
+        }
+        response = requests.request("POST", url, headers=headers)
+        response = response.json()
+        if self.environment_mode == "sandbox":
+            self.api_key_collections = str(response["apiKey"])
+        username, password = self.collections_apiuser, self.api_key_collections
+        self.basic_authorisation_collections = str(encode(username, password))
 
     def momotoken(self):
         url = f"{self.accurl}/collection/token/"
         headers = {
             'Ocp-Apim-Subscription-Key': self.collections_subkey,
-            'Authorization': self.basic_authorisation_collections
+            'Authorization': str(self.basic_authorisation_collections)
         }
-        response = requests.post(url, headers=headers)
-        return response.json()
+        response = requests.request("POST", url, headers=headers)
+        authorization_token = response.json()
+        return authorization_token
 
-    def pay_view(request):
-        if request.method == 'POST':
-            amount = request.POST.get('amount')
-            currency = request.POST.get('currency')
-            txt_ref = request.POST.get('txt_ref')
-            phone_number = request.POST.get('phone_number')
-            payermessage = request.POST.get('payermessage')
+    def momopay(self, amount, currency, txt_ref, phone_number, payermessage):
+        uuidgen = str(uuid.uuid4())
+        url = f"{self.accurl}/collection/v1_0/requesttopay"
+        payload = json.dumps({
+            "amount": amount,
+            "currency": currency,
+            "externalId": txt_ref,
+            "payer": {
+                "partyIdType": "MSISDN",
+                "partyId": phone_number
+            },
+            "payerMessage": payermessage,
+            "payeeNote": payermessage
+        })
+        headers = {
+            'X-Reference-Id': uuidgen,
+            'X-Target-Environment': self.environment_mode,
+            'Ocp-Apim-Subscription-Key': self.collections_subkey,
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer "+str(self.momotoken()["access_token"])
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        context = {"response": response.status_code, "ref": uuidgen}
+        return context
 
-            pay = PayClass()
-            context = pay.momopay(amount, currency, txt_ref, phone_number, payermessage)
-            return JsonResponse(context)
-        else:
-            return render(request, 'pay.html')
+    def verifymomo(self, txn):
+        url = f"{self.accurl}/collection/v1_0/requesttopay/{txn}"
+        headers = {
+            'Ocp-Apim-Subscription-Key': self.collections_subkey,
+            'Authorization':  "Bearer "+str(self.momotoken()["access_token"]),
+            'X-Target-Environment': self.environment_mode,
+        }
+        response = requests.request("GET", url, headers=headers)
+        json_respon = response.json()
+        return json_respon
 
-    def verify_view(request, txn):
-        pay = PayClass()
-        context = pay.verifymomo(txn)
-        return JsonResponse(context)
-
-    # Check momo collections balance
-    def momobalance(request):
-        pay = PayClass()
-        context = pay.momobalance()
-        return JsonResponse(context)
+    def momobalance(self):
+        url = f"{self.accurl}/collection/v1_0/account/balance"
+        headers = {
+            'Ocp-Apim-Subscription-Key': self.collections_subkey,
+            'Authorization':  "Bearer "+str(self.momotoken()["access_token"]),
+            'X-Target-Environment': self.environment_mode,
+        }
+        response = requests.request("GET", url, headers=headers)
+        json_respon = response.json()
+        return json_respon
 
 def index(request):
-    pay = PayClass()
-    token = pay.momotoken()
-    return JsonResponse(token)
+    pay_class = PayClass()
+    pay_class.create_api_user()
+    pay_class.create_api_key()
+    return render(request, 'mtnmo.html')
