b37c19113d962d29f6633abcedaf5113cddf8434
Ivar-7 <leank6071@gmail.com>
2024-04-26 09:05:21 +0300

    first request

--- mtnmo/views.py
+++ mtnmo/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .momo import PayClass
 
 def index(request):
-    # momo = PayClass()
-    # token = momo.momotoken()
+    callPay = PayClass.momopay("100", "EUR", "1234Test", "0968793843", "Donate to charity")
+    print(callPay["response"])
+    print(callPay["ref"]) 
     return render(request, 'mtnmo.html')
 
