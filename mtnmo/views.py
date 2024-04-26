fc23cc29423548e950f2bef2c54c0b7fdbb75aec
Ivar-7 <leank6071@gmail.com>
2024-04-26 10:07:20 +0300

    request from view

--- mtnmo/views.py
+++ mtnmo/views.py
@@ -1,9 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .momo import PayClass
 
 def index(request):
-    callPay = PayClass.momopay("100", "EUR", "1234Test", "0968793843", "Donate to charity")
-    print(callPay["response"])
-    print(callPay["ref"]) 
     return render(request, 'mtnmo.html')
 
+def pay(request):
+    if request.method == 'POST':
+        phonenumber = str(request.POST.get('phone_number'))
+        amount = str(request.POST.get('amount'))
+        try:
+            callPay = PayClass.momopay(amount, "EUR", "1234Test", phonenumber, "Donate to charity")
+            return render(request, 'pay.html', {'response': callPay["response"], 'ref': callPay["ref"]})
+        except KeyError as e:
+            return HttpResponse(f"Error: Key '{e}' not found in the response.")
+        except Exception as e:
+            return HttpResponse(f"An error occurred: {str(e)}")
+    else:
+        return render(request, 'pay.html')
