730c16e450f24057c693c1d780374c8eaf06a5d5
Ivar-7 <leank6071@gmail.com>
2024-04-28 12:21:06 +0300

    transaction model

--- mtnmo/models.py
+++ mtnmo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.Model):
+    financial_transaction_id = models.CharField(max_length=100)
+    external_id = models.CharField(max_length=100, blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    currency = models.CharField(max_length=10, blank=True, null=True)
+    party_id_type = models.CharField(max_length=50, blank=True, null=True)
+    party_id = models.CharField(max_length=50, blank=True, null=True)
+    payer_message = models.TextField(blank=True, null=True)
+    payee_note = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=50)
--- mtnmo/views.py
+++ mtnmo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .collection import Collection
 from .disbursement import Disbursement
+from .models import Transaction
 
 def index(request):
     return render(request, 'mtnmo/mtnmo.html')
@@ -8,16 +9,39 @@ def index(request):
 def collection(request):
     coll = Collection()
     response = coll.requestToPay(amount="600", phone_number="0966456787", external_id="123456789")
-    status_resposne = coll.getTransactionStatus(response['ref'])
-    print(status_resposne)
+    status_response = coll.getTransactionStatus(response['ref'])
+
+    transaction = Transaction(
+        financial_transaction_id=status_response['financialTransactionId'],
+        external_id=status_response['externalId'],
+        amount=status_response['amount'],
+        currency=status_response['currency'],
+        party_id_type=status_response['payer']['partyIdType'],
+        party_id=status_response['payer']['partyId'],
+        payer_message=status_response['payerMessage'],
+        payee_note=status_response['payeeNote'],
+        status=status_response['status'],
+    )
+    transaction.save()
 
     return render(request, 'mtnmo/pay.html', {"response": response})
 
 def disbursement(request):
     disbur = Disbursement()
     response = disbur.transfer(amount="600", phone_number="0966456787", external_id="123456789", payee_note="dd", payer_message="dd", currency="EUR")
-    transfer_status_res = disbur.getTransactionStatus(response['transaction_ref'])
-    if transfer_status_res['status'] == "SUCCESS":
-        print('success')
+    transfer_status_res = disbur.getTransactionStatus(response['ref'])
 
-    return transfer_status_res['status']
+    transaction = Transaction(
+        financial_transaction_id=transfer_status_res['financialTransactionId'],
+        external_id=transfer_status_res['externalId'],
+        amount=transfer_status_res['amount'],
+        currency=transfer_status_res['currency'],
+        party_id_type=transfer_status_res['payer']['partyIdType'],
+        party_id=transfer_status_res['payer']['partyId'],
+        payer_message=transfer_status_res['payerMessage'],
+        payee_note=transfer_status_res['payeeNote'],
+        status=transfer_status_res['status'],
+    )
+    transaction.save()
+
+    return render(request, 'mtnmo/disburse.html', {"response": response})
