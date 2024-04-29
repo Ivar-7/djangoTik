faff1d9f2a1dac55d6112ab4eaf61d9754327472
Ivar-7 <leank6071@gmail.com>
2024-04-29 15:31:47 +0300

    make requirements.txt

--- djangoTik/settings.py
+++ djangoTik/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'djangoTik.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            # 'NAME': 'your_database_name',
+            # 'USER': 'your_database_user',
+            # 'PASSWORD': 'your_database_password',
+            # 'HOST': 'localhost',
+            # 'PORT': '5432',
+        }
+    }
 
 
 # Password validation
