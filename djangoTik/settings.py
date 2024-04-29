4822340a234b2624c5f8700468e3eab6ee61fab0
Ivar-7 <leank6071@gmail.com>
2024-04-29 17:04:49 +0300

    remove debug

--- djangoTik/settings.py
+++ djangoTik/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-if5#1wvd-+f6cy81p&(uyi!0gc=icou535eya%(m2u+%s8002#'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://paying-17a2954b0bab.herokuapp.com/", "127.0.0.1"]
 
 
 # Application definition
