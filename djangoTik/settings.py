b113af547df495aec29da6c589b1b3dc3ce0a121
Kevin Mwangi <127428309+Ivar-7@users.noreply.github.com>
2024-04-29 18:14:47 +0300

    Hosted heroku (#2)
    * make requirements.txt
    
    * made requirements
    
    * added static
    
    * domain url as config
    
    * added procfile
    
    * added procfile reqs
    
    * update procfile
    
    * remove debug
    
    * change allowed hosts
    
    * change allowed hosts 2
    
    * allow specific hosts
    
    * allow all hosts
    
    * added dj_database_url
    
    * debug false
    
    * correct settings
    
    * debug false
    
    * ALLOW ALL HOSTS
    
    * not allowing host

--- djangoTik/settings.py
+++ djangoTik/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
+import dj_database_url # type: ignore
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-if5#1wvd-+f6cy81p&(uyi!0gc=icou535eya%(m2u+%s8002#'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = ["*.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -76,13 +79,14 @@ WSGI_APPLICATION = 'djangoTik.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -122,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
