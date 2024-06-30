08185a4533d85b25ce98ad68f0f35b20eb82aef7
Ivar-7 <leank6071@gmail.com>
2024-06-30 11:56:52 +0300

    flutter app payment

--- mpesa/views.py
+++ mpesa/views.py
@@ -1,93 +1,76 @@
 from django.shortcuts import render
-
-# Import necessary mpesa modules
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
+from requests.auth import HTTPBasicAuth
 import requests
 import datetime
 import base64
-from requests.auth import HTTPBasicAuth
 from decouple import config
+import json
+from django.views.decorators.csrf import csrf_exempt
+from .models import MpesaTransaction
 
 def index(request):
     return render(request, 'mpesa/index.html')
 
-# Define your Django view
+@csrf_exempt
 def mpesa_payment(request):
     if request.method == 'POST':
-        phone = str(request.POST.get('phone'))
-        # amount = str(request.POST.get('amount'))
-        # GENERATING THE ACCESS TOKEN
+        data = json.loads(request.body)
+        phone = data.get('phone')
+        amount = data.get('amount')
+
         consumer_key = config('CONSUMER_KEY')
         consumer_secret = config('CONSUMER_SECRET')
-
-        api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"  # AUTH URL
+        api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
         r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
-
         data = r.json()
-        access_token = "Bearer" + ' ' + data['access_token']
+        access_token = "Bearer " + data['access_token']
 
-        #  GETTING THE PASSWORD
         timestamp = datetime.datetime.today().strftime('%Y%m%d%H%M%S')
         passkey = config('PASSKEY')
         business_short_code = config('BUSINESS_SHORTCODE')
-        data = business_short_code + passkey + timestamp
-        encoded = base64.b64encode(data.encode())
+        data_to_encode = business_short_code + passkey + timestamp
+        encoded = base64.b64encode(data_to_encode.encode())
         password = encoded.decode('utf-8')
 
-        # BODY OR PAYLOAD
         payload = {
-            "BusinessShortCode": "174379",
-            "Password": "{}".format(password),
-            "Timestamp": "{}".format(timestamp),
+            "BusinessShortCode": business_short_code,
+            "Password": password,
+            "Timestamp": timestamp,
             "TransactionType": "CustomerPayBillOnline",
-            "Amount": "1",  # use 1 when testing
-            "PartyA": phone,  # change to your number
-            "PartyB": "174379",
+            "Amount": amount,
+            "PartyA": phone,
+            "PartyB": business_short_code,
             "PhoneNumber": phone,
-            "CallBackURL": "https://mydomain.com/path",
+            "CallBackURL": "https://yourdomain.com/mpesa/callback",
             "AccountReference": "account",
-            "TransactionDesc": "account"
+            "TransactionDesc": "payment"
         }
 
-        # POPULATING THE HTTP HEADER
         headers = {
             "Authorization": access_token,
             "Content-Type": "application/json"
         }
 
-        url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"  # C2B URL
-
         try:
-            response = requests.post(url, json=payload, headers=headers)
+            response = requests.post("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest", json=payload, headers=headers)
             response_data = response.json()
-            print(response_data)
-            return render(request, 'mpesa/mpesa_payment_form.html', {'response': response_data})
+            return JsonResponse(response_data)
         except KeyError as e:
-            return HttpResponse(f"Error: Key '{e}' not found in the response.")
+            return JsonResponse({"status": "error", "message": f"Key '{e}' not found in the response."})
         except Exception as e:
-            return HttpResponse(f"An error occurred: {str(e)}")
-    else:
-        # Handle GET requests
-        # Render a form or any other content you want to display for GET requests
-        return render(request, 'mpesa/mpesa_payment_form.html')
-
-# Logging
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import MpesaTransaction
-import json
+            return JsonResponse({"status": "error", "message": str(e)})
+    return JsonResponse({"status": "error", "message": "Invalid request method."})
 
 @csrf_exempt
 def mpesa_callback(request):
     if request.method == 'POST':
         data = json.loads(request.body)
 
-        # Extract the callback metadata items if present
         items = {}
         if 'CallbackMetadata' in data['Body']['stkCallback']:
             items = {item['Name']: item['Value'] for item in data['Body']['stkCallback']['CallbackMetadata']['Item']}
 
-        # Create a new MpesaTransaction object and save it to the database
         MpesaTransaction.objects.create(
             merchant_request_id=data['Body']['stkCallback']['MerchantRequestID'],
             checkout_request_id=data['Body']['stkCallback']['CheckoutRequestID'],
@@ -100,6 +83,4 @@ def mpesa_callback(request):
         )
 
         return JsonResponse({'result_code': 0, 'result_desc': 'Success'})
-
-    else:
-        return JsonResponse({'result_code': 1, 'result_desc': 'Failed, not a POST request'})
+    return JsonResponse({'result_code': 1, 'result_desc': 'Failed, not a POST request'})
